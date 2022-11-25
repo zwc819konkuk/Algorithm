c0963cbb0cba8b46b93cd21f5c68b00a82ce949c
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-11-25 20:44:33 +0900

    对数器

--- 新手班/Code02_Sort.java
+++ 新手班/Code02_Sort.java
@@ -1,5 +1,7 @@
 package 新手班;
 
+import java.util.Arrays;
+
 public class Code02_Sort {
 
     /**
@@ -115,10 +117,68 @@ public class Code02_Sort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {5, 1, 9, 4, 1, 6, 9, 6, 6,};
+        int testTime = 500000;
+        int maxSize = 100; //随机数组的长度
+        int maxValue = 100;//-100~100
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            int[] arr1 = copyArray(arr);
+            int[] arr2 = copyArray(arr);
+            insertSort01(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice" : "fuck");
+        int[] arr = generateRandomArray(maxSize, maxValue);
         printArray(arr);
-        selectionSort(arr);
+        insertSort01(arr);
         printArray(arr);
     }
+
+    private static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    private static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) ((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+
 
 }
