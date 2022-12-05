7b0798518df5493f0f6eec07e6c32ae2016aeff1
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-12-05 18:06:29 +0900

    deque queue stack

---
+++ codes02/Code03_LinkedListToQueueAndStack.java
@@ -0,0 +1,124 @@
+/**
+ * 用链表实现队列和栈
+ */
+public class Code03_LinkedListToQueueAndStack {
+
+    public static class Node<V> {
+        public V value;
+        public Node<V> next;
+
+        public Node(V v) {
+            value = v;
+            next = null;
+        }
+    }
+
+    public static class MyQueue<V> {
+        private Node<V> head;
+        private Node<V> tail;
+        private int size;
+
+        public MyQueue() {
+            this.head = null;
+            this.tail = null;
+            this.size = 0;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        /**
+         * 存数据
+         *
+         * @param value
+         */
+        public void offer(V value) {
+            Node<V> cur = new Node<V>(value);
+            if (tail == null) {
+                head = cur;
+                tail = cur;
+            } else {
+                tail.next = cur;
+                tail = cur;
+            }
+            size++;
+        }
+
+        /**
+         * 弹出数据
+         *
+         * @return
+         */
+        public V poll() {
+            V ans = null;
+            if (head != null) {
+                ans = head.value;
+                head = head.next;
+                size--;
+            }
+            if (head == null) {
+                tail = null;
+            }
+            return ans;
+        }
+
+        /**
+         * 不弹出数据 只拿数据
+         */
+        public V peek() {
+            V ans = null;
+            if (head != null) {
+                ans = head.value;
+            }
+            return ans;
+        }
+    }
+
+    public static class MyStack<V> {
+        private Node<V> head;
+        private int size;
+
+        public MyStack() {
+            head = null;
+            size = 0;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void push(V value) {
+            Node<V> cur = new Node<V>(value);
+            if (head == null) {
+                head = cur;
+            } else {
+                cur.next = head;
+                head = cur;
+            }
+            size++;
+        }
+
+        public V pop(V value) {
+            V ans = null;
+            if (head != null) {
+                ans = head.value;
+                head = head.next;
+                size--;
+            }
+            return ans;
+        }
+
+        public V peek() {
+            return head != null ? head.value : null;
+        }
+    }
+}
---
+++ codes02/Code04_DoubleLinkedListToDeque.java
@@ -0,0 +1,100 @@
+/**
+ * 双向链表实现双端队列
+ */
+public class Code04_DoubleLinkedListToDeque {
+
+
+    public static class Node<V> {
+
+        public V value;
+        public Node<V> last;
+        public Node<V> next;
+
+        public Node(V v) {
+            value = v;
+            last = null;
+            next = null;
+        }
+    }
+
+    public static class MyDeque<V> {
+        private Node<V> head;
+        private Node<V> tail;
+        private int size;
+
+        public MyDeque() {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void pushHead(V value) {
+            Node<V> cur = new Node<V>(value);
+            if (head == null) {
+                head = cur;
+                tail = cur;
+            } else {
+                cur.next = head;
+                head.last = cur;
+                head = cur;
+            }
+            size++;
+        }
+
+        public void pushTail(V value) {
+            Node<V> cur = new Node<V>(value);
+            if (head == null) {
+                head = cur;
+                tail = cur;
+            } else {
+                tail.next = cur;
+                cur.last = tail;
+                tail = cur;
+            }
+            size++;
+        }
+
+        public V pollHead() {
+            V ans = null;
+            if (head == null) {
+                return ans;
+            }
+            size--;
+            ans = head.value;
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                head = head.next;
+                head.last = null;
+            }
+            return ans;
+        }
+
+        public V pollTail() {
+            V ans = null;
+            if (head == null) {
+                return ans;
+            }
+            size--;
+            ans = tail.value;
+            if (head == tail) {
+                head = null;
+                tail = null;
+            } else {
+                tail = tail.last;
+                tail.next = null;
+            }
+            return ans;
+        }
+    }
+
+}
